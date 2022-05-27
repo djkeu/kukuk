8a3e1203ccdbffc72af71cd7659a07e53ee4e217
djkeu <mrkooij@gmail.com>
2022-05-27 09:55:27 +0200

    Attempt to add delay to resultTextView

--- app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
+++ app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
