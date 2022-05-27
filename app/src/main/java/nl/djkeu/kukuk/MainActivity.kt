6112f2c7e35ba8862d17429a84652c4a1f203e67
djkeu <mrkooij@gmail.com>
2022-05-27 09:56:10 +0200

    Remove abudant import

--- app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
+++ app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
