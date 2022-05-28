2fab13706a39903b782a972283cc0e0f3dd1accb
djkeu <mrkooij@gmail.com>
2022-05-28 16:32:28 +0200

    Rename minutelyAlarm()

--- app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
+++ app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : AppCompatActivity() {
             showKuku()
         }
 
-        fun minutelyAlarms() {
+        fun minutelyAlarm() {
             val getCurrentTime = Calendar.getInstance().time
             val formatter = SimpleDateFormat("ss", Locale.getDefault())
             val currentTime = formatter.format(getCurrentTime)
@@ -50,7 +50,7 @@ class MainActivity : AppCompatActivity() {
 
         // Callback needed
         // see: Dev/kotlin/callBack01.kt for code
-        minutelyAlarms()
+        minutelyAlarm()
     }
 }
 
