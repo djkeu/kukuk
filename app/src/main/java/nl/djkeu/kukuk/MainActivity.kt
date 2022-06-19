4e600c307fb4022b874d7e4353ea5dde4e049e02
djkeu <mrkooij@gmail.com>
2022-06-19 16:56:04 +0200

    Add call for quarterlyAlarms() to runBlocking

--- app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
+++ app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
@@ -29,6 +29,7 @@ class MainActivity : AppCompatActivity() {
         runBlocking {
             // Not looping
             minutelyAlarms()
+            quarterlyAlarms()
             //kukuOnce()
         }
 
