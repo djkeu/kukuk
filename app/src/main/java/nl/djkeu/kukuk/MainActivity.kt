232a22d735799e9f0a8c7b62755d05b815ab2ab8
djkeu <mrkooij@gmail.com>
2022-06-21 09:10:56 +0200

    Restore minutelyAlarm()s

--- app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
+++ app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
@@ -59,8 +59,93 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    // Refactor
+    private fun minutelyAlarms() {
+        // For testing, to be removed when the alarms work
+        val getCurrentTime = Calendar.getInstance().time
+        val formatter = SimpleDateFormat("mm:ss", Locale.getDefault())
+        val currentTime = formatter.format(getCurrentTime)
 
-        private fun quarterlyAlarms() {
+        for (i in 1..59) {
+            if (i < 11) {
+                val formattedMinute = String.format("%02d", i)
+                val hour = "${formattedMinute}:00"
+
+                if (hour == currentTime) {
+                    kukuTimes(i)
+                }
+            } else if (i < 21) {
+                val times = i - 10
+                val formattedMinute = String.format("%02d", i)
+                val hour = "${formattedMinute}:00"
+
+                if (hour == currentTime) {
+                    kukuTimes(times)
+                }
+            } else if (i < 31) {
+                val times = i - 20
+                val formattedMinute = String.format("%02d", i)
+                val hour = "${formattedMinute}:00"
+
+                if (hour == currentTime) {
+                    kukuTimes(times)
+                }
+            } else if (i < 41) {
+                val times = i - 30
+                val formattedMinute = String.format("%02d", i)
+                val hour = "${formattedMinute}:00"
+
+                if (hour == currentTime) {
+                    kukuTimes(times)
+                }
+            } else if (i < 51) {
+                val times = i - 40
+                val formattedMinute = String.format("%02d", i)
+                val hour = "${formattedMinute}:00"
+
+                if (hour == currentTime) {
+                    kukuTimes(times)
+                }
+            } else {
+                val times = i - 40
+                val formattedMinute = String.format("%02d", i)
+                val hour = "${formattedMinute}:00"
+
+                if (hour == currentTime) {
+                    kukuTimes(times)
+                }
+            }
+
+        }
+    }
+
+
+    /* Backup
+    private fun minutelyAlarms() {
+        // For testing, to be removed when the alarms work
+        val getCurrentTime = Calendar.getInstance().time
+        val formatter = SimpleDateFormat("ss", Locale.getDefault())
+        val currentTime = formatter.format(getCurrentTime)
+
+        // val minutes = arrayOf("05", "10", "20",
+        //            "25", "35", "40", "50", "55")
+
+        val minutes = arrayOf("05", "15", "25",
+            "35", "45", "55")
+
+        // val minutes = arrayOf("10", "25",
+        //    "40", "55")
+
+        if (currentTime in minutes) {
+            kukuOnce()
+        }
+    }
+    */
+
+
+
+
+    private fun quarterlyAlarms() {
                 val getCurrentTime = Calendar.getInstance().time
                 val formatter = SimpleDateFormat("mm:ss", Locale.getDefault())
                 val currentTime = formatter.format(getCurrentTime)
