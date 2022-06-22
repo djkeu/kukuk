cffc4c90b50020a36f3b0ba6ac5a2194c892227f
djkeu <mrkooij@gmail.com>
2022-06-22 21:44:00 +0200

    Use minutely array instead of quarterly

--- app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
+++ app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
@@ -47,7 +47,6 @@ class MainActivity : AppCompatActivity() {
         val resId = resources.getIdentifier("keukuk", "raw", packageName)
         val mediaPlayer = MediaPlayer.create(this, resId)
         mediaPlayer.start()
-        mediaPlayer.pause()
 
         // Show kuku
         val resultTextView: TextView = findViewById(R.id.textView2)
@@ -69,16 +68,22 @@ class MainActivity : AppCompatActivity() {
 
     // Call kukuOnce() every 15 minutes
     private fun quarterlyAlarms() {
-                val getCurrentTime = Calendar.getInstance().time
-                val formatter = SimpleDateFormat("mm:ss", Locale.getDefault())
-                val currentTime = formatter.format(getCurrentTime)
+        val getCurrentTime = Calendar.getInstance().time
+        val formatter = SimpleDateFormat("mm:ss", Locale.getDefault())
+        val currentTime = formatter.format(getCurrentTime)
 
-                val quarters = arrayOf("15:00", "30:00", "45:00")
+        // val quarters = arrayOf("15:00", "30:00", "45:00")
+        val quarters = arrayOf( "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00",
+            "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00",
+            "30:00", "31:00", "32:00", "33:00", "34:00", "35:00", "36:00", "37:00", "38:00", "39:00",
+            "40:00", "41:00", "42:00", "43:00", "44:00", "45:00", "46:00", "47:00", "48:00", "49:00",
+            "50:00", "51:00", "52:00", "53:00", "54:00", "55:00", "56:00", "57:00", "58:00", "59:00")
 
-                if (currentTime in quarters) {
-                    kukuOnce()
-                }
-            }
+
+        if (currentTime in quarters) {
+            kukuOnce()
+        }
+    }
 
     // Call kukuTimes every hour
     private fun hourlyAlarms() {
