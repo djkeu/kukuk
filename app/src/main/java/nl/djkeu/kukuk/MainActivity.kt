2d2ad96c681fd144e6d4c567e5159250674c4aa5
djkeu <mrkooij@gmail.com>
2022-06-22 13:52:41 +0200

    Run runAlarms() in while loop

--- app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
+++ app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
@@ -34,7 +34,9 @@ class MainActivity : AppCompatActivity() {
         }
 
         runBlocking {
-               runAlarms()
+            while (true) {
+                runAlarms()
+            }
         }
 }
 
