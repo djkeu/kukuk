4be5c29ccb20c41288b1f449ec15a225d3b63117
djkeu <mrkooij@gmail.com>
2022-06-24 12:40:07 +0200

    Set up minutelyAlarms() for testing of kukuTimes()

--- app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
+++ app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import kotlinx.coroutines.*
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.text.Typography.times
 
 
 class MainActivity : AppCompatActivity() {
@@ -28,8 +29,9 @@ class MainActivity : AppCompatActivity() {
             stopAlarms()
             job = scope.launch {
                 while (true) {
-                    quarterlyAlarms()
-                    hourlyAlarms()
+                    //quarterlyAlarms()
+                    //hourlyAlarms()
+                    minutelyAlarms()
                     delay(1000)
                 }
             }
@@ -102,6 +104,39 @@ class MainActivity : AppCompatActivity() {
                 i - 12
             }
             checkHours(times)
+        }
+    }
+
+    // Call kukuTimes every minute
+    private fun minutelyAlarms() {
+        val getCurrentTime = Calendar.getInstance().time
+        val formatter = SimpleDateFormat("mm:ss", Locale.getDefault())
+        val currentTime = formatter.format(getCurrentTime)
+
+        for (i in 1..59) {
+            fun checkMinutes(times: Int) {
+                val formattedMinute = String.format("%02d", i)
+                val minute = "${formattedMinute}:00"
+
+                if (minute == currentTime) {
+                    kukuTimes(times)
+                }
+            }
+
+            val times = if (i < 11) {
+                i - 0
+            } else if (i < 21) {
+                i - 10
+            } else if (i < 31) {
+                i - 20
+            } else if (i < 41) {
+                i - 30
+            } else if (i < 51) {
+                i - 40
+            } else {
+                i - 50
+            }
+            checkMinutes(times)
         }
     }
 }
