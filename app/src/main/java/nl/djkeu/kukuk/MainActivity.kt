df64d84b9f7754837df00c413ce54923feea74dc
djkeu <mrkooij@gmail.com>
2022-10-27 15:20:56 +0200

    Delete obsolete versions of minutelyAlarms()

--- app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
+++ app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
@@ -146,41 +146,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-    // Obsolete Minutely alarms
-    private fun minutelyAlarmsOld() {
-        val getCurrentTime = Calendar.getInstance().time
-        val formatter = SimpleDateFormat("mm:ss", Locale.getDefault())
-        val currentTime = formatter.format(getCurrentTime)
-
-        for (i in 0..59) {
-            fun checkMinutes(times: Int) {
-                val formattedMinute = String.format("%02d", i)
-                val minute = "${formattedMinute}:00"
-
-                if (minute == currentTime) {
-                    kukuTextTimes(times)
-                    // kukuSoundTimes(times)
-                }
-            }
-
-            val times = if (i == 0) {
-                i + 10
-            } else if (i < 11) {
-                i - 0
-            } else if (i < 21) {
-                i - 10
-            } else if (i < 31) {
-                i - 20
-            } else if (i < 41) {
-                i - 30
-            } else if (i < 51) {
-                i - 40
-            } else {
-                i - 50
-            }
-
-            checkMinutes(times)
-        }
-    }
 }
