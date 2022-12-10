82494899180e7896638fbe2e092015cc67f87810
djkeu <mrkooij@gmail.com>
2022-12-10 11:53:22 +0100

    Add ToDo comment to kukuScope

--- app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
+++ app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // ToDo: this scope doesn't fix the UI freezes
         // Start and stop the alarms
         val kukuScope = MainScope()
         var job: Job? = null
