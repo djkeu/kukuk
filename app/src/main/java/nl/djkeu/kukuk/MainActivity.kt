943d3f34c939d07ef281d926520b3de7fec49573
djkeu <mrkooij@gmail.com>
2023-02-17 01:04:26 +0100

    Comment out Toast import

--- app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
+++ app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.os.Looper
 import android.os.Handler
 import android.widget.TextView
-import android.widget.Toast
+// import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
 import java.text.SimpleDateFormat
