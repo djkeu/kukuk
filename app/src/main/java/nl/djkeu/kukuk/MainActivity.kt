4dd92279a2a05a580697e4f9f476a012b84f3940
djkeu <mrkooij@gmail.com>
2023-02-17 14:26:25 +0100

    Combine kukuTextTimes() and kukuSoundTimes()

--- app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
+++ app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
@@ -84,17 +84,21 @@ class MainActivity : AppCompatActivity() {
 
 
     // Show kuku text multiple times
+    /*
     @Suppress("unused")
     private fun kukuTextTimes(times: Int) {
+        // Moved to kukuSoundTimes
         for (i in 1..times) {
             kukuTextOnce()
         }
     }
+    */
 
 
     // Play kuku sound multiple times
     private suspend fun kukuSoundTimes(times: Int) {
         for (i in 1..times) {
+            kukuTextOnce()
             kukuSoundOnce()
         }
     }
