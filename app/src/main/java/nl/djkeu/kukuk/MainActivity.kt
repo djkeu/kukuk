a14fa781fa934e1a2f3d3be28c613bbbee2dad9c
djkeu <mrkooij@gmail.com>
2023-02-19 11:45:13 +0100

    Replace if..else with when in hourlyAlarms()

--- app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
+++ app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
@@ -122,12 +122,10 @@ class MainActivity : AppCompatActivity() {
         val formatter = SimpleDateFormat("hh:mm:ss", Locale.getDefault())
         val currentTime = formatter.format(getCurrentTime)
 
-        // ToDo: Use when statement instead of long if..else ladder
         for (i in 1..24) {
-            val times = if (i < 13) {
-                i - 0
-            } else {
-                i - 12
+            val times = when {
+                i < 13 -> i - 0
+                else -> i - 12
             }
 
             val formattedHour = String.format("%02d", i)
