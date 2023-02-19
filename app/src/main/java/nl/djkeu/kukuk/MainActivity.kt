05729b055283861d3bfa0f0a233d41ee31dfd3ad
djkeu <mrkooij@gmail.com>
2023-02-19 12:05:53 +0100

    Use try-catch in kukuSoundOnce()

--- app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
+++ app/src/main/java/nl/djkeu/kukuk/MainActivity.kt
@@ -74,19 +74,17 @@ class MainActivity : AppCompatActivity() {
 
     // Play kuku sound once
     private suspend fun kukuSoundOnce() {
+        try {
+            val resourceId = resources.getIdentifier("keukuk", "raw", packageName)
+            val kukuPlayer = MediaPlayer.create(this, resourceId)
 
-        // ToDo: Add try-catch block around MediaPlayer.create call, catch exceptions
-        // ToDo: W/MediaPlayer-JNI: MediaPlayer finalized without being released
-
-        val resourceId = resources.getIdentifier("keukuk", "raw", packageName)
-        val kukuPlayer = MediaPlayer.create(this, resourceId)
-
-        kukuPlayer.start()
-        delayWithMillis(1000)
-        kukuPlayer.stop()
-        delayWithMillis(200)
+            kukuPlayer.start()
+            delayWithMillis(1200)
+            kukuPlayer.release() // Release the MediaPlayer instance
+        } catch (e: Exception) {
+            // Handle the exception here
+        }
     }
-
 
     // Kuku multiple times
     private suspend fun kukuMultipleTimes(times: Int) {
